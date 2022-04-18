901d98bf683d0160e2a5b9ae31bda0dc4e12a6d4
HunJer93 <jhunton22@gmail.com>
2022-04-18 18:01:35 -0400

    Made updates to SentimentAnalysis SQS input and started setup for SentimentAnalysis output to DynamoDB. Made additions to docker-compose file and edits to prevent collisions with previous project container.

--- Sentiment_Analysis/index.py
+++ Sentiment_Analysis/index.py
@@ -1,27 +1,49 @@
 import json
+from multiprocessing.connection import Client
 import re
 import textblob as TextBlob
 import json
 import pandas as pd
+import boto3
+import logging
+from botocore.exceptions import ClientError
 
+# global constants
+
+# sqs client
+SQS = boto3.client('sqs')
+# logger (and configuration)
+LOGGER = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
+
+DYNAMO_CLIENT = boto3.client('dynamodb')
 
 # handle the import json from SQS
 def lambda_handler(event, context):
   
-  # create array to load json
+  # local host endpoint url for sqs
+  SQS_LOCAL_HOST_ENDPOINT = 'http://localhost:4566/queue/sentiment-analysis-queue'
+  
+  # receive sqs queue
+  messages = receive_queue_message(SQS_LOCAL_HOST_ENDPOINT)
+  
+  # create array to hold the incoming sqs messages
   tweet_array_json = []
-  # cycle each record from the event
-  for record in event.records:
+  
+  # cycle the messages and load into array
+  for msg in messages['Messages']:
+    msg_body = msg['body']
+    receipt_handle = msg['ReceiptHandle']
     
-    # unpack the message body from the body
-    message_body = json.loads(record['body'])
+    # log the message body for testing
+    LOGGER.info(f'The message body: {msg_body}')
     
-    # unpack the message from the message body
-    message = json.loads(message_body['Message'])
-    
-    # load the message into the json array
-    tweet_array_json.append(message)
-    
+    # put the message contents into the array
+    tweet_array_json.append(msg_body) 
+   
+  # ######################################################
+  # Might need to edit the data from below....
+  
   # create the data structure for the data_frame
   d = {'Tweet': [tweet.text for tweet in tweet_array_json], 
       'Re-Tweet Count': [tweet.retweet_count for tweet in tweet_array_json],
@@ -33,10 +55,10 @@ def lambda_handler(event, context):
   data_frame = pd.DataFrame(data = d)
   
   #sort by retweet count
-  dataFrame = dataFrame.sort_values(['Re-Tweet Count', 'Favorite Count'], ascending= False)
+  data_frame = data_frame.sort_values(['Re-Tweet Count', 'Favorite Count'], ascending= False)
   
   # clean the tweets from the column 'Tweets
-  dataFrame['Tweet'] = dataFrame['Tweet'].apply(textScrubber)
+  data_frame['Tweet'] = data_frame['Tweet'].apply(textScrubber)
 
   # create two columns for subjectivity and polarity
   data_frame['Subjectivity'] = data_frame['Tweet'].apply(getSubjectivity)
@@ -44,14 +66,47 @@ def lambda_handler(event, context):
 
   # create a new column for the analysis. This will be done in the database for the project. 
   data_frame['Analysis'] = data_frame['Polarity'].apply(getAnalysis)
-
-
-  # store info in database
   
-  # create API endpoint to send info/CRUD
+  # send the data frame to export to dynamo
+  export_to_dynamo(data_frame)
   
-  # return front end response (look up with API Gateway)
+  return 0
 
+  
+# receive_queue_message retrieves messages from the SQS queue
+def receive_queue_message(queue_url):
+  try:
+    response = SQS.receive_message(QueueUrl=queue_url)
+  except ClientError:
+    LOGGER.exception(
+      f'Could not receive the message from - {queue_url}.')
+    raise
+  else:
+    return response
+  
+# delete_queue_message deletes the queue message when the information is extracted
+def delete_queue_message(queue_url,receipt_handle):
+  try:
+    response = SQS.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+  except ClientError:
+    LOGGER.exception(f'Could not delete the message from - {queue_url}.')
+    raise
+  else:
+    return response
+  
+# export_to_dynamo exports the data frame created to a dynamo DB
+def export_to_dynamo(data_frame):
+
+  # for each record within the data frame, load the payload
+  for record in data_frame:
+    # try to post the record to the database
+    try:
+      'POST'(DYNAMO_CLIENT, record)
+    except Exception as e:
+      LOGGER.error(e)
+    else:
+      LOGGER.error('Post to database failed for an unknown reason')
+      
 # clean the raw tweet info using a text scrubber method
 def textScrubber(text):
   # use a regular expression to look for @ mentions 
--- Sentiment_Analysis/test_index.py
+++ Sentiment_Analysis/test_index.py
@@ -24,3 +24,48 @@ def test_textScrubber_remove_hyperlink():
     text = 'RT@TestPerson check this out! #thisisneat #wow https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     
     assert index.textScrubber(text) == 'check this out! thisisneat wow'
+    
+# when a payload is sent to the lambda_handler, it processes the payload
+def test_lambda_handler():
+    # dummy event
+    event = {
+    "resource": "/",
+    "path": "/",
+    "httpMethod": "GET",
+    "requestContext": {
+        "resourcePath": "/",
+        "httpMethod": "GET",
+        "path": "/Prod/",
+    },
+    "headers": {
+        "accept": "text/html",
+        "accept-encoding": "gzip, deflate, br",
+        "Host": "xxx.us-east-2.amazonaws.com",
+        "User-Agent": "Mozilla/5.0",
+    },
+    "multiValueHeaders": {
+        "accept": [
+            "text/html"
+        ],
+        "accept-encoding": [
+            "gzip, deflate, br"
+        ],
+    },
+    "queryStringParameters": {
+        "postcode": 12345
+        },
+    "multiValueQueryStringParameters": "NULL",
+    "pathParameters": "NULL",
+    "stageVariables": "NULL",
+    "body": {
+        "Message": {
+        'Query': 'Michael Bolton, Lonely Island',
+        'Num_Tweets' : 10
+    }},
+    "isBase64Encoded": "False"
+    }
+    
+    context = " "
+    response = 200
+    actual = index.lambda_handler(event, context)['ResponseMetadata']['HTTPStatusCode']
+    assert  actual == response
